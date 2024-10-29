6667671049f8d8b637c958c8f2332d2982c5b7cd
anonymous <anonymous@domain.com>
2024-10-29 17:53:24 +0900

    [front] S11P31A609-173 feat: 비트봇 기본 기능 개발

--- FE/trai/src/components/BitBot/index.jsx
+++ FE/trai/src/components/BitBot/index.jsx
@@ -1,15 +1,73 @@
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from "styled-components";
+import data from '@data/bitbotData.json';
 
 
 import { IoCloseCircleOutline } from "react-icons/io5";
 
 const BitbotModal = ({ onClose }) => {
-  return (
+
+  const [currentStep, setCurrentStep] = useState(data.root);
+  const [history, setHistory] = useState([]);
+
+  useEffect(()=> {
+    setHistory([]);
+  }, []);
+
+  const handleOptionClick = (option) => {
+    const newHistoryItem = {
+      question: currentStep.question,
+      answer: option.answer || null,
+      selectedOption: option.label,
+    };
+
+    setHistory([...history, newHistoryItem]);
+
+    if(option.next) {
+      setCurrentStep(data[option.next]);
+    } else if(option.answer) {
+      setCurrentStep({
+        question: null,
+        answer: option.answer,
+        options:[]
+      })
+    }
+  }
+
+   return (
     <ModalOverlay>
       <ModalContainer>
         <CloseButton onClick={onClose}><IoCloseCircleOutline /></CloseButton>
-        <p>모달이지</p>
+
+        <HistoryContainer>
+          {history.map((item, index) => (
+            <HistoryItem key={index}>
+              <Question>{item.question}</Question>
+              <SelectedOption>{item.selectedOption}</SelectedOption>
+              {item.answer && <Answer>{item.answer}</Answer>}
+            </HistoryItem>
+          ))}
+        </HistoryContainer>
+
+        <div className='mt-10 border border-black p-2'>
+          {!currentStep.answer && (
+            <div className='flex gap-2 items-center mt-1 mb-3'>
+              <div className='bg-trai-mint w-[14px] h-[14px] rounded-full'></div>
+              <Question>{currentStep.question}</Question>
+            </div>
+          )}
+          {currentStep.answer && (
+            <Answer>{currentStep.answer}</Answer>
+          )}
+          <Options>
+            {currentStep.options.map((option, index) => (
+              <OptionButton key={index} onClick={()=> handleOptionClick(option)}>
+                {option.label}
+              </OptionButton>
+            ))}
+          </Options>
+        </div>
       </ModalContainer>
     </ModalOverlay>
   )
@@ -22,10 +80,8 @@ BitbotModal.propTypes = {
 
 const ModalOverlay = styled.div`
   position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
+  top: 19%;
+  left: 22%;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -33,27 +89,78 @@ const ModalOverlay = styled.div`
 `;
 
 const ModalContainer = styled.div`
+  display: flex;
+  flex-direction: column;
   background-color: white;
   padding: 20px;
-  border-radius: 10px;
-  width: 300px;
-  height: 300px;
+  border-radius: 20px;
+  width: 400px;
+  height: 600px;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  box-shadow: 3px 3px 10px var(--trai-disabled);
   position: relative;
 `;
 
 const CloseButton = styled.button`
   background-color: transparent;
-  color: black;
+  color: var(--trai-mint);
   border: none;
-  font-size: 24px;
+  font-size: 32px;
   position: absolute;
-  top: 10px;
-  right: 10px;
+  top: 15px;
+  right: 15px;
+  margin-bottom: 20px;
   cursor: pointer;
 `;
+
+const HistoryContainer = styled.div`
+  margin-top: 30px;
+  max-height: 500px;
+  overflow-y: auto;
+  margin-bottom: 10px;
+`;
+
+const HistoryItem = styled.div`
+  margin-bottom: 8px;
+  gap: 10px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Question = styled.div`
+  font-size: 16px;
+`
+
+const Answer = styled.div`
+  font-size: 16px;
+  background-color: var(--trai-disabled);
+  padding: 7px;
+  border-radius: 10px;
+`;
+
+const SelectedOption = styled.div`
+  background-color: var(--trai-mint);
+  color: var(--trai-white);
+  display: flex;
+  justify-content: end;
+  font-size: 16px;
+  padding: 5px 10px;
+  border-radius: 20px;
+`;
+
+const Options = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+
+`
+
+const OptionButton = styled.button`
+  border: 1px solid var(--trai-mint);
+  color: var(--trai-mint);
+  padding: 5px 10px;
+  border-radius: 20px;
+`
 
 
 export default BitbotModal;
