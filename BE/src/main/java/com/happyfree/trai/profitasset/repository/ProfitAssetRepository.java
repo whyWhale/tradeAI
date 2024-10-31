2f992c068630e47305bbe891c9c20073d2d3cb76
anonymous <anonymous@domain.com>
2024-10-31 13:58:25 +0900

    [back] S11P31A609-144 feat: 투자 손익 상세
    - 자산 현황 화면

--- BE/src/main/java/com/happyfree/trai/profitasset/repository/ProfitAssetRepository.java
+++ BE/src/main/java/com/happyfree/trai/profitasset/repository/ProfitAssetRepository.java
@@ -4,15 +4,17 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.happyfree.trai.profitasset.entity.ProfitAssetHistory;
 import com.happyfree.trai.user.entity.User;
 
 public interface ProfitAssetRepository extends JpaRepository<ProfitAssetHistory, Long> {
-	List<ProfitAssetHistory> findByUser(User user);
+	Page<ProfitAssetHistory> findByUser(User user, Pageable pageable);
 
-	Optional<ProfitAssetHistory> findByUserAndSettlementDate(@Param("user") User user, @Param("yesterday") LocalDate yesterday);
+	Optional<ProfitAssetHistory> findByUserAndSettlementDate(@Param("user") User user,
+		@Param("yesterday") LocalDate yesterday);
 }
