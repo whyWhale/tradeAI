5b8f8c784803d772e227163bcdf888c3e74d4711
anonymous <anonymous@domain.com>
2024-11-01 15:35:15 +0900

    [back] S11P31A609-242 feat: 일별 거래 내역 조회

--- BE/src/main/java/com/happyfree/trai/investment/repository/InvestmentHistoryRepository.java
+++ BE/src/main/java/com/happyfree/trai/investment/repository/InvestmentHistoryRepository.java
@@ -1,12 +1,20 @@
 package com.happyfree.trai.investment.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.happyfree.trai.investment.entity.InvestmentHistory;
 import com.happyfree.trai.user.entity.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface InvestmentHistoryRepository extends JpaRepository<InvestmentHistory, Long> {
 	List<InvestmentHistory> findByUserOrderByCreatedAt(User user);
+
+	@Query("SELECT i " +
+			"FROM InvestmentHistory i " +
+			"WHERE i.user = :user AND DATE(i.orderCreatedAt) = :date")
+	List<InvestmentHistory> findByUserAndDate(@Param("user") User user, @Param("date") LocalDate date);
 }
