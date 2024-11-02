d7cab11089932c2e749c1496f8e269f9ae731999
anonymous <anonymous@domain.com>
2024-11-02 18:47:41 +0900

    [back] S11P31A609-243 feat: 매일 자정에 Scheduler 추가

--- BE/src/main/java/com/happyfree/trai/profitasset/scheduler/ProfitAssetScheduler.java
+++ BE/src/main/java/com/happyfree/trai/profitasset/scheduler/ProfitAssetScheduler.java
@@ -1,17 +1,22 @@
 package com.happyfree.trai.profitasset.scheduler;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.happyfree.trai.profitasset.service.ProfitAssetService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+
 @Component
+@RequiredArgsConstructor
 public class ProfitAssetScheduler {
-    @Autowired
-    ProfitAssetService profitAssetService;
+
+    private final ProfitAssetService profitAssetService;
 
     @Scheduled(cron = "0 0 0 * * *") // 매일 자정
-    public void saveProfitAssetHistory() {
+    public void saveProfitAssetHistory() throws UnsupportedEncodingException, NoSuchAlgorithmException, JsonProcessingException {
         profitAssetService.save();
     }
 }
