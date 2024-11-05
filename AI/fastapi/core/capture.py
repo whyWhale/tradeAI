17a672545baed414f0b481dd6fef9b1e6bd57317
anonymous <anonymous@domain.com>
2024-11-05 16:50:39 +0900

    [ai] S11P31A609-279 fix: Chrome 대신 Chromium 사용 테스트

--- AI/fastapi/core/capture.py
+++ AI/fastapi/core/capture.py
@@ -22,6 +22,7 @@ def capture_chart_screenshot():
         raise EnvironmentError("Unsupported operating system. Only Windows and Linux are supported.")
 
     chrome_options = Options()
+    chrome_options.binary_location = "/usr/bin/chromium-browser"  # Chromium의 경로 설정
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
