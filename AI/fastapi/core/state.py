ec73da794cf0ebe1d673003f463532dba5388a66
anonymous <anonymous@domain.com>
2024-11-06 17:52:23 +0900

    [ai] S11P31A609-273 fix: 랭그래프 병렬 처리

--- AI/fastapi/core/state.py
+++ AI/fastapi/core/state.py
@@ -4,7 +4,7 @@ from langgraph.graph.message import add_messages
 
 
 class State(BaseModel):
-    metadata: Optional[dict] = None # 분석 완료 날짜
+    data_info: Optional[dict] = None # 분석 완료 날짜
     user_info: Optional[dict] = None # 유저 아이디, 주문 가능 금액, 보유 비트코인 원화
     messages: Annotated[list, add_messages]
     fng: Optional[dict] = None
