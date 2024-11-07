f6c81e4210cb2cc6f5038b542571bc9fd74c4b88
anonymous <anonymous@domain.com>
2024-11-07 17:45:42 +0900

    Update index.js

--- FE/trai/src/store/index.js
+++ FE/trai/src/store/index.js
@@ -1,7 +1,8 @@
 // src/store/index.js
 import { configureStore } from '@reduxjs/toolkit';
 import BTCDataReducer from './BTCDataSlice'
-import rootReducer from './reducers'; // 리듀서 모음 (필요에 따라 설정)
+import authReducer from './authSlice';
+
 
 const store = configureStore({
   reducer: {
