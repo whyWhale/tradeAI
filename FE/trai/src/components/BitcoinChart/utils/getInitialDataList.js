cffbc67a6903792b50a15531ae4b043a0ddd5c23
anonymous <anonymous@domain.com>
2024-11-12 16:55:37 +0900

    [front] S11P31A609-346 fix: 차트 데이터 수신 디버깅

--- FE/trai/src/components/BitcoinChart/utils/getInitialDataList.js
+++ FE/trai/src/components/BitcoinChart/utils/getInitialDataList.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getInitialDataList = (unit) => {
   return axios.get(`${import.meta.env.VITE_BASE_URL}/api/upbit/candles/minutes/${unit}`, {
-    params: {
+      params: {
       market: 'KRW-BTC',
       to: new Date(+new Date() + 3240 * 10000).toISOString().replace("T", " ").replace(/\..*/, ''),
       count: 720 // 12시간 = 720분
@@ -10,6 +10,7 @@ const getInitialDataList = (unit) => {
   })
       .then(res => res.data)
       .then((data) => {
+        console.log("차트 데이터: "+data)
         return data.map((item) => {
           const { opening_price, low_price, high_price, trade_price, timestamp, candle_acc_trade_volume } = item;
           return {
@@ -26,6 +27,7 @@ const getInitialDataList = (unit) => {
       })
       .then((arr) => arr.reverse()) // 차트 데이터는 가장 오래된 데이터부터 최신 데이터 순으로 정렬
       .catch(err => {
+        console.log("차트 데이터 받아오기 실패")
         console.error(err);
         return []; // 에러 발생 시 빈 배열 반환
       });
