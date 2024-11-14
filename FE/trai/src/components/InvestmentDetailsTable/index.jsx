81257e9bf5702421fd3ea225b4549422c1b32aa7
anonymous <anonymous@domain.com>
2024-11-14 17:11:56 +0900

    Update index.jsx

--- FE/trai/src/components/InvestmentDetailsTable/index.jsx
+++ FE/trai/src/components/InvestmentDetailsTable/index.jsx
@@ -11,7 +11,7 @@ const InvestmentDetailsTable = () => {
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
-
+  
   const getProfitColor = (value) => {
     if (value > 0) return '#C84A31';  // 빨간색
     if (value < 0) return '#1261C4';  // 파란색
@@ -46,17 +46,17 @@ const InvestmentDetailsTable = () => {
           : ""}
       </td>
       
-      <td style={{ textAlign: "right" }}>
+      <td style={{ textAlign: "right", color: getProfitColor(item.dailyProfitRatio)}}>
         {item.dailyProfitRatio != null ? `${item.dailyProfitRatio}%` : ""}
       </td>
       
-      <td style={{ textAlign: "right" }}>
+      <td style={{ textAlign: "right", color: getProfitColor(item.accumulationProfitAndLoss) }}>
         {item.accumulationProfitAndLoss !== undefined && item.accumulationProfitAndLoss !== null 
           ? `₩${Number(item.accumulationProfitAndLoss).toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 })}`
           : ""}
       </td>
       
-      <td style={{ textAlign: "right" }}>
+      <td style={{ textAlign: "right", color: getProfitColor(item.accumulationProfitRatio) }}>
         {item.accumulationProfitRatio != null ? `${item.accumulationProfitRatio}%` : ""}
       </td>
       
@@ -75,40 +75,6 @@ const InvestmentDetailsTable = () => {
   ))}
 </tbody>
 
-=======
-          {data.map((item, index) => (
-            <tr key={index}>
-              <td>{item.settlementDate}</td>
-              <td style={{ color: getProfitColor(item.dailyProfitAndLoss) }}>
-                  {item.dailyProfitAndLoss !== undefined && item.dailyProfitAndLoss !== null 
-                  ? Number(item.dailyProfitAndLoss).toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 })
-                  : ""}
-              </td>
-              <td style={{ color: getProfitColor(item.dailyProfitRatio) }}>
-                {item.dailyProfitRatio != null ? `${item.dailyProfitRatio}%` : ""}
-              </td>
-              <td style={{ color: getProfitColor(item.accumulationProfitAndLoss) }}>
-                {item.accumulationProfitAndLoss !== undefined && item.accumulationProfitAndLoss !== null 
-                  ? Number(item.accumulationProfitAndLoss).toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 })
-                  : ""}
-              </td>
-              <td style={{ color: getProfitColor(item.accumulationProfitRatio) }}>
-                {item.accumulationProfitRatio != null ? `${item.accumulationProfitRatio}%` : ""}
-                </td>
-              <td>
-                {item.startingAssets !== undefined && item.startingAssets !== null 
-                  ? Number(item.startingAssets).toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 })
-                  : ""}
-              </td>
-              <td>
-                {item.endingAssets !== undefined && item.endingAssets !== null 
-                  ? Number(item.endingAssets).toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 })
-                  : ""}
-              </td>
-            </tr>
-          ))}
-        </tbody>
->>>>>>> FE/trai/src/components/InvestmentDetailsTable/index.jsx
       </table>
 
       {/* 페이지네이션 */}
